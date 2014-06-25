3b92515804884d4009309b14bbba03e68ad15d91
Nikolay Fadin <kuvaldis1988@gmail.com>
2014-06-26 01:40:06 +0300

    add css build dist

--- Client/gulpfile.js
+++ Client/gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require("gulp"),
-    clean = require("gulp-clean");
+    src = gulp.src,
+    dest = gulp.dest,
+    clean = require("gulp-clean"),
+    less = require("gulp-less"),
+    autoprefixer = require("gulp-autoprefixer"),
+    concat = require("gulp-concat"),
+    minifyCss = require('gulp-minify-css');
 
 var jsAll = {
     normal: 'all.js',
@@ -7,27 +13,58 @@ var jsAll = {
 };
 var paths = {
     src: {
-        base: 'src'
+        base: 'src',
+        less: {
+            files: 'src/less/**/*.less'
+        }
     },
     build: {
         base: 'build',
-        lib: 'dist/vendor'
+        css: {
+            result: 'build/css',
+            files: 'build/css/**/*.css'
+        }
     },
     dist: {
-        base: 'dist'
+        base: 'dist',
+        css: {
+            file: 'app.min.css',
+            result: 'dist/css'
+        }
+    },
+    vendor: {
+        css: [
+            'vendor/bootstrap/dist/css/bootstrap.css'
+        ]
     }
 };
 
 gulp.task("clean:build", function () {
-    return gulp.src([paths.build.base])
+    return src([paths.build.base])
         .pipe(clean())
 });
 
 gulp.task("clean:dist", function () {
-    return gulp.src([paths.dist.base])
+    return src([paths.dist.base])
         .pipe(clean())
 });
 
 gulp.task("clean", ["clean:dist", "clean:build"]);
 
-gulp.task("build", ["clean"]);
+gulp.task("build:css", function () {
+    return src(paths.src.less.files)
+        .pipe(less())
+        .pipe(autoprefixer())
+        .pipe(dest(paths.build.css.result))
+});
+
+gulp.task("dist:css", ["build:css"], function () {
+    return src([].concat(paths.vendor.css).concat(paths.build.css.files))
+        .pipe(concat(paths.dist.css.file))
+        .pipe(minifyCss())
+        .pipe(dest(paths.dist.css.result))
+});
+
+gulp.task("build", ["build:css"]);
+
+gulp.task("dist", ["dist:css"]);
